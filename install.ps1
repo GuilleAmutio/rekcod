8721b2691f36c6402be8029a62157ce4185ffd85
Guillermo Amutio Duarte <guilleamutio@gmail.com>
2022-01-01 19:53:57 +0100

    Divide in multiple scripts

--- install.ps1
+++ install.ps1
@@ -1,5 +1,7 @@
 #Requires -RunAsAdministrator
 
+## TODO Keep everything clean after installation
+
 #region Tools
 #endregion
 
@@ -17,42 +19,24 @@ dockerd --register-service
 
 #region WSL
 ## TODO Set alias for rekcod=docker and rekcod-compose=docker-compose
+## TODO Allow user to select WSL installation folder
 ## TODO Check WSL is already enabled
+
 ## Enable WSL
 wsl --install -d Ubuntu-20.04
 wsl --set-default-version 2
 
-## Basic configuration
-wsl --exec sudo apt get update
-wsl --exec sudo apt upgrade -y
+## Download wsl-install.sh script on the wsl distro script and execute it
 
-## Configure WSL distro to use systemd
-wsl --exec sudo apt install -yqq fontconfig daemonize
 
-# The command below has a problem when running from Windows
-wsl --exec sudo echo "SYSTEMD_PID=$(ps -efw | grep '/lib/systemd/systemd --system-unit=basic.target$' | grep -v unshare | awk '{print $2}')
- 
-if [ -z "$SYSTEMD_PID" ]; then
-   sudo /usr/bin/daemonize /usr/bin/unshare --fork --pid --mount-proc /lib/systemd/systemd --system-unit=basic.target
-   SYSTEMD_PID=$(ps -efw | grep '/lib/systemd/systemd --system-unit=basic.target$' | grep -v unshare | awk '{print $2}')
-fi
- 
-if [ -n "$SYSTEMD_PID" ] && [ "$SYSTEMD_PID" != "1" ]; then
-    exec sudo /usr/bin/nsenter -t $SYSTEMD_PID -a su - $LOGNAME
-fi" > /etc/profile.d/00-wsl2-systemd.sh
+## Download wsl-systemd.sh script on the wsl distro and execute it
 
+
+## Restart WSL distro to start using systemd
 wsl -t Ubuntu-20.04
-## Install Dockerd runtime
-wsl --exec sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
-wsl --exec curl -fsSL https://download.docker.com/linux/ubuntu/gpg | wsl --exec sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-# OUT FILE ISSUE
-# wsl --exec echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | wsl --exec sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-wsl --exec sudo apt-get update
 
-# These packages MUST be marked as hold to prevent updates without previous testing
-wsl --exec sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+## Check WSL distro uses systemd
 
-## Configure WSL to launch dockerd on startup
-wsl --exec sudo systemctl enable docker.service
-wsl --exec sudo systemctl enable containerd.service
+## Download wsl-docker.sh script on the wsl distro and execute it
+
 #endregion
