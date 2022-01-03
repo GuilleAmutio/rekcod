ac1496de9ed1ec2e71ff2399735ec5274fbd8d3f
Guillermo Amutio Duarte <guilleamutio@gmail.com>
2022-01-03 23:36:08 +0100

    Core installation done

--- install.ps1
+++ install.ps1
@@ -1,59 +1,72 @@
 #Requires -RunAsAdministrator
 
+# All TODOs
 ## TODO Keep everything clean after installation
+## TODO Check every download and execution works as expected
+## TODO Allow user to select DestinationPath. Ask user for InstallationPath
+## TODO Check if dockerd is already installed before install
+## TODO Check WSL is already enabled
+## TODO Check the distro is in version 2
+## TODO Specify which WSL distro should execute the commands. Something like 'wsl -d rekcod-wsl --exec echo "Hello world"
+## TODO Add environment variable pointing to docker-compose.exe
+## TODO Path of script for alias should be parametrized
+## TODO Check if the InstallationPath does already exist. Even if the default path is used
+## !BUG Docker is only started and managed from an admin prompt. Check behaviour and user-groups. Create one if necessary
 
-#region Tools
-#endregion
+# variables
+$RekcodInstallationPath = "C:\rekcod"
+
+# Introduction about rekcod and ask user for installation path telling the default path
+Write-Host @"
+Welcome to rekcod installation wizard!
+rekcod is a tool developed to guide the installation of docker for Windows and Linux container.
+This tool uses a WSL distribution based on Ubuntu-20.04 and the binary of Docker for Windows.
+The default installation path is "C:/rekcod". Would you like to change it? (Y/N)
+"@
+
+
+### Select folder to install
+mkdir $RekcodInstallationPath
 
 #region Windows
-## TODO Allow user to select DestinationPath
-## TODO Set alias for rekcod=docker and rekcod-compose=docker-compose
-## TODO Check if dockerd is already installed 
 
 ## Docker CLI
 curl.exe -o docker.zip -LO https://download.docker.com/win/static/stable/x86_64/docker-20.10.8.zip 
-Expand-Archive docker.zip -DestinationPath C:\
+Expand-Archive docker.zip -DestinationPath $RekcodInstallationPath
 Remove-Item docker.zip
-[Environment]::SetEnvironmentVariable("Path", "$($env:path);C:\docker", [System.EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("Path", "$($env:path);$RekcodInstallationPath\docker", [System.EnvironmentVariableTarget]::Machine)
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 dockerd --register-service
 
 ## docker-compose
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile C:\docker\docker-compose.exe
-
+Invoke-WebRequest "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $RekcodInstallationPath\docker\docker-compose.exe
 
 #endregion
 
 #region WSL
-## TODO Set alias for rekcod=docker and rekcod-compose=docker-compose
-## TODO Allow user to select WSL installation folder
-## TODO Check WSL is already enabled
-## TODO Use variables for paths
-## TODO Check the distro is in version 2
-## TODO Specify which WSL distro should execute the commands
+
 ## Create WSL distro
 
-### Select folder to install
-mkdir C:/rekcod
-
 ### Copy scripts and files
-Copy-Item ./scripts/ C:/rekcod/ # Have to be tested
-Copy-Item uninstall.ps1 C:/rekcod # Have to be tested
+Copy-Item ./scripts/ $RekcodInstallationPath -Recurse
+Copy-Item uninstall.ps1 $RekcodInstallationPath
+Copy-Item start.ps1 $RekcodInstallationPath
+Copy-Item stop.ps1 $RekcodInstallationPath
 
-wsl --import rekcod-wsl C:/rekcod tools/rekcod-wsl.tar 
+wsl --import rekcod-wsl $RekcodInstallationPath tools/rekcod-wsl.tar 
 wsl --set-version rekcod-wsl 2
 
-## Download wsl-install.sh script on the wsl distro script and execute it
+## Call wsl-install.sh script from inside the WSl distro
 wsl --exec ./scripts/wsl-install.sh
 
-## Download wsl-systemd.sh script on the wsl distro and execute it
+## Call wsl-systemd.sh script from inside the WSl distro
 wsl --exec ./scripts/wsl-systemd.sh
 
 ## Restart WSL distro to start using systemd
 wsl -t rekcod-wsl
 
-## Download wsl-docker.sh script on the wsl distro and execute it
+## Call wsl-docker.sh script from inside the WSl distro
 wsl --exec ./scripts/wsl-docker.sh
 
 #endregion
@@ -61,12 +74,10 @@ wsl --exec ./scripts/wsl-docker.sh
 #region Configuration
 
 ## Set 'rekcod-start' as an alias for the start.ps1 script
-## TODO Path of script for alias should be parametrized
-Set-Alias rekcod-start C:\docker\scripts\start.ps1
+Set-Alias rekcod-start $RekcodInstallationPath\start.ps1
 
 ## Set 'rekcod-off' as an alias for the stop.ps1 script
-## TODO Path of script for alias should be parametrized
-Set-Alias rekcod-shutdown C:\docker\scripts\start.ps1
+Set-Alias rekcod-shutdown $RekcodInstallationPath\stop.ps1
 
 ## Set 'rekcod' as an alias for 'docker'
 Set-Alias rekcod docker
@@ -75,3 +86,6 @@ Set-Alias rekcod docker
 Set-Alias rekcod-compose docker-compose
 
 #endregion
+
+# Refresh environment variables
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
