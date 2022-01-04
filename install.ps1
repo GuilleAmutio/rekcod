856d2bcd9829be1726a87ceaff0c4203f275c5dd
Guillermo Amutio Duarte <guilleamutio@gmail.com>
2022-01-04 02:06:34 +0100

    Allow user to select installation path

--- install.ps1
+++ install.ps1
@@ -3,30 +3,61 @@
 # All TODOs
 ## TODO Keep everything clean after installation
 ## TODO Check every download and execution works as expected
-## TODO Allow user to select DestinationPath. Ask user for InstallationPath
 ## TODO Check if dockerd is already installed before install
 ## TODO Check WSL is already enabled
 ## TODO Check the distro is in version 2
 ## TODO Specify which WSL distro should execute the commands. Something like 'wsl -d rekcod-wsl --exec echo "Hello world"
-## TODO Add environment variable pointing to docker-compose.exe
-## TODO Path of script for alias should be parametrized
-## TODO Check if the InstallationPath does already exist. Even if the default path is used
+## TODO Write-Host for each step with a yellow color
+## TODO Clear console for each menu iteration
 ## !BUG Docker is only started and managed from an admin prompt. Check behaviour and user-groups. Create one if necessary
 
-# variables
-$RekcodInstallationPath = "C:\rekcod"
+# Variables
+$RekcodInstallationPath = "C:/rekcod"
+$Answer = "N"
+$TmpPath
 
 # Introduction about rekcod and ask user for installation path telling the default path
 Write-Host @"
 Welcome to rekcod installation wizard!
 rekcod is a tool developed to guide the installation of docker for Windows and Linux container.
 This tool uses a WSL distribution based on Ubuntu-20.04 and the binary of Docker for Windows.
-The default installation path is "C:/rekcod". Would you like to change it? (Y/N)
 "@
+do {
+    Write-Host 'The default installation path is '$RekcodInstallationPath''
+    $Answer = Read-Host -Prompt 'Would you like to change it? Default is no (Y/N)'
+    
+    if($Answer -ne "Y" -and $Answer -ne "N" ) {
+        Write-Host 'Please, choose yes (Y) or no (N)'
+    }
+    elseif($Answer -eq "Y") {
+        do {
+            Write-Host 'Write the absolute path for rekcod installation. The path MUST exists.'
+            $TmpPath = Read-Host -Prompt 'Please, select where rekcod will be installed'
 
+            Write-Host 'Rekcod will be installed in '$TmpPath''
+            $Answer = Read-Host -Prompt 'Is this correct? (Y/N)'
+
+            if($Answer -ne "Y" -and $Answer -ne "N" ) {
+                Write-Host 'Please, choose yes (Y) or no (N)'
+            }
+            elseif($Answer -eq "Y") {
+                if (-not (Test-Path $TmpPath)) {
+                    Write-Host 'The path indicated does not exist. Please, select a valid one.'
+                    $Answer = "N"
+                }
+                else {
+                    $RekcodInstallationPath = $TmpPath + "/rekcod"
+                    Write-Host 'Path is valid. Rekcod will be installed at '$RekcodInstallationPath''                    
+                }
+            }
+        } while ($Answer -ne "Y")
+    }
+} while ($Answer -ne "Y" -and $Answer -ne "N")
 
 ### Select folder to install
-mkdir $RekcodInstallationPath
+if (-not (Test-Path $RekcodInstallationPath)){
+    mkdir $RekcodInstallationPath
+}
 
 #region Windows
 
