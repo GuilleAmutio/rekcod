79165c40b25b1de0b7d387994bf1b212965c02a0
Guillermo Amutio Duarte <guillermo.amutio-duarte@capgemini.com>
2022-01-13 10:52:32 +0100

    Write start script

--- start.ps1
+++ start.ps1
@@ -4,7 +4,7 @@
 Start-Service docker
 
 # Start WSL distro
-
-## Expose daemon to Windows host
+wsl -d rekcod-wsl --exec ./scripts/wsl-start.sh
 
 ## Create link to use from Windows
+docker context create lin --docker host=tcp://127.0.0.1:2375
