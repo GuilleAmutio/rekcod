39265aa91f638ab8c314ac0d8754ff6b428dab4e
Guillermo Amutio Duarte <guillermo.amutio-duarte@capgemini.com>
2022-01-13 17:27:02 +0100

    Change comment

--- scripts/wsl-start.sh
+++ scripts/wsl-start.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-# Maybe this is something that can be done once and not every start
+# Expose daemon to Windows
 sudo cp /lib/systemd/system/docker.service /etc/systemd/system/
 sudo sed -i 's/\ -H\ fd:\/\//\ -H\ fd:\/\/\ -H\ tcp:\/\/127.0.0.1:2375/g' /etc/systemd/system/docker.service
 sudo systemctl daemon-reload
