837ab9353dc4e5f22553c0518afb117cc3bfecb6
Guillermo Amutio Duarte <guillermo.amutio-duarte@capgemini.com>
2022-01-13 10:55:57 +0100

    Stop rekcod

--- stop.ps1
+++ stop.ps1
@@ -1,10 +1,10 @@
 #Requires -RunAsAdministrator
 
 # Stop current containers
-
+docker kill $(docker ps -q)
 
 # Shutdown WSL distro
-
+wsl -t rekcod-wsl
 
 # Stop dockerd service
-
+Stop-Service docker
