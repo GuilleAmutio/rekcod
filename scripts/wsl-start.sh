e6e02c7442a57204f50079ad00e7396505e0e4f6
Guillermo Amutio Duarte <guilleamutio@gmail.com>
2022-01-15 13:13:10 +0100

    Write script inside WSL

--- scripts/wsl-start.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-
-# Expose daemon to Windows
-sudo cp /lib/systemd/system/docker.service /etc/systemd/system/
-sudo sed -i 's/\ -H\ fd:\/\//\ -H\ fd:\/\/\ -H\ tcp:\/\/127.0.0.1:2375/g' /etc/systemd/system/docker.service
-sudo systemctl daemon-reload
-sudo systemctl restart docker.service
