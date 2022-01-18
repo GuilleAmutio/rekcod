0a6d9b520047c6b767a90ff5b5aae08c3ea46c21
Guillermo Amutio Duarte <guillermo.amutio-duarte@capgemini.com>
2022-01-18 21:26:36 +0100

    The job is started by a powershell thats being closed, so it cannot be found

--- pwsh-scripts/pwsh-stop.ps1
+++ pwsh-scripts/pwsh-stop.ps1
@@ -6,7 +6,6 @@ param()
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 # Shutdown WSL distro
-Stop-Job -Name rekcod-wsl
 wsl -t rekcod-wsl
 
 # Stop dockerd service
